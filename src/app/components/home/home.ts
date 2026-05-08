e4a14cae9af0cbbdd7daa212b42d4ef0815d1f3f
guigosf <guisf2007@gmail.com>
2026-05-07 22:27:18 -0300

    first commit

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+public nomeProduto: string = "Curso de Angular";
+      anuncio: string = `O ${this.nomeProduto} está em promoção`;
+      idProduto: number = 123;
+      precoProduto: number = 2.59;
+      promocao: boolean = true;
+      foto: string = "img/crud.png";
+
+      testeVariaveis(): void{
+        //variáveis de string com concatenação
+        //this.anuncio + `O` + this.nomeProduto + `está em promoção`;
+        console.log(`Nome do Produto: `, this.nomeProduto);
+        console.log(`Anuncio: `,this.anuncio);
+        console.log(`ID: `, this.idProduto);
+        console.log(`Preço: `, this.precoProduto);
+        console.log(`Promoção: `, this.promocao);
+
+
+      }
+}
